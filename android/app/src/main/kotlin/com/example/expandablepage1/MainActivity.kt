90cfa44ac1b1414db1b277f72a25ca30cd35bbae
abhayhari17@gmail.com <abhayhari17@gmail.com>
2023-10-04 11:41:13 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/expandablepage1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expandablepage1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar:AppBar(title: Text('Home'),
+        backgroundColor: Colors.green,),
+      body: Center(
+        child: SizedBox(
+          width: 200.0,
+          height: 140.0,
+          child: AutoSizeText(
+            'Hello',
+            style: TextStyle(fontSize: 30.0),
+            maxLines: 3,
+          ),
+        ),
+      ),
+    );
+  }
+}
